a10c82a60d20cf3ef915ccb6930b500a7b907971
pukun1115-tech <pukun1115@gmail.com>
2026-06-24 20:40:40 +0900

    Refactor player movement with collision checks

--- main.js
+++ main.js
@@ -29,25 +29,49 @@ function loop(){
 
 function move(){
     const speed = 0.5;
+    let nextPlayerX = player.pos.x;
+    let nextPlayerY = player.pos.y;
     
     if(keys["w"]){
-        player.pos.y += speed;
+        nextPlayerY += speed;
     }
 
     if(keys["s"]){
-        player.pos.y -= speed;
+        nextPlayerY -= speed;
     }
 
     if(keys["d"]){
-        player.pos.x += speed;
+        nextPlayerX += speed;
     }
 
     if(keys["a"]){
-        player.pos.x -= speed;
+        nextPlayerX -= speed;
     }
+
+    if(!checkCollision(nextPlayerX,player.pos.y)) player.pos.x = nextPlayerX;
+    if(!checkCollision(player.pos.x,nextPlayerY)) player.pos.y = nextPlayerY;
 }
 
 function playerdraw(){
     ctx.fillStyle = "white";
     ctx.fillRect(((canvas.width) / 2) - (player.size / 2), ((canvas.height) / 2) - (player.size / 2), player.size, player.size);
 }
+
+function checkCollision(newX,newY){
+    for(let my = 0;my < 80;my++){
+        for(let mx = 0;mz < 8;mx++){
+            if(maze[my][mx]===1){
+                const wx = mx * 16;
+                const wy = my * 16;
+                if(newX > wx &&
+                   newX < wx + 16 &&
+                   newY > wy &&
+                   newY < wy +16){
+                 return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+

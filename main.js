91dd5ababe0c1dff6fead8ede82a07df706e5ef9
pukun1115-tech <pukun1115@gmail.com>
2026-06-24 19:21:34 +0900

    Refactor resize, move, and loop functions

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ window.addEventListener("resize",resize);
 
 resize();
 
-function resize()
-{    
+function resize(){    
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
@@ -15,31 +14,40 @@ const keys = {};
 document.addEventListener("keydown", e => keys[e.key] = true);
 document.addEventListener("keyup", e => keys[e.key] = false);
 
-const player = 
-{
+const player = {
     pos : {x : 0, y : 0},
     size : 8
 };
 
 loop();
 
-function loop()
-{
+function loop(){
     move();
     playerdraw();
     requestAnimationFrame(loop);
 }
 
-function move()
-{
-    if(keys["w"])
-    {
-        player.pos.y +=0;
+function move(){
+    const speed = 0.5;
+    
+    if(keys["w"]){
+        player.pos.y += speed;
+    }
+
+    if(keys["s"]){
+        player.pos.y -= speed;
+    }
+
+    if(keys["d"]){
+        player.pos.x += speed;
+    }
+
+    if(keys["a"]){
+        player.pos.x -= speed;
     }
 }
 
-function playerdraw()
-{
+function playerdraw(){
     ctx.fillStyle = "white";
     ctx.fillRect(((canvas.width) / 2) - (player.size / 2), ((canvas.height) / 2) - (player.size / 2), player.size, player.size);
 }

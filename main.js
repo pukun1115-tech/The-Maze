059c7bc587693c7319b30c175178f19266dadb68
pukun1115-tech <pukun1115@gmail.com>
2026-06-25 19:04:01 +0900

    ookinahennka

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const canvas = document.getElementById("canvas");
-
-const ctx = gameCanvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 window.addEventListener("resize", resize);
 
 resize();
 
 function resize(){    
-    gameCanvas.width = window.innerWidth;
-    gameCanvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
 const keys = {};
@@ -16,24 +15,23 @@ document.addEventListener("keydown", e => keys[e.key] = true);
 document.addEventListener("keyup", e => keys[e.key] = false);
 
 const player = {
-    pos : {x : 320, y : 80},//左上の座標
+    pos : {x : 320, y : 64},//左上の座標
     size : 32
 };
-
-const mazeSize = 64;
 
 loop();
 
 function loop(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    
+
     move();
-    playerDraw();
-    mazeDraw();
+    playerdraw();
+    mazedraw();
     requestAnimationFrame(loop);
 }
+
 function move(){
-    const speed = 0.5;
+    const speed = 2;
     let nextPlayerX = player.pos.x;
     let nextPlayerY = player.pos.y;
     
@@ -57,21 +55,24 @@ function move(){
     if(!checkCollision(player.pos.x, nextPlayerY)) player.pos.y = nextPlayerY;
 }
 
-function playerDraw(){
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect((canvas.width) / 2, (canvas.height) / 2, player.size, player.size);
+function playerdraw(){
+    ctx.fillStyle = "#0040ff";
+    ctx.fillRect((canvas.width) / 2 - 1, (canvas.height) / 2 - 1, player.size + 2, player.size + 2);
 }
 
 function checkCollision(newX, newY){
-    for(let my = 0; my < 8; my++){
-        for(let mx = 0; mx < 8; mx++){
+
+    for(let my = 0; my < maze.length; my++){
+        for(let mx = 0; mx < maze[0].length; mx++){
             if(maze[my][mx]===1){
                 const wx = mx * mazeSize;
                 const wy = my * mazeSize;
-                if(newX > wx &&
-                   newX < wx + mazeSize &&
-                   newY > wy &&
-                   newY < wy + mazeSize){
+                if(
+                    newX < mx * mazeSize + mazeSize&&
+                    newX + player.size>mx * mazeSize&&
+                    newY < my * mazeSize + mazeSize &&
+                    newY + player.size > my * mazeSize
+                ){
                  return true;
                 }
             }
@@ -79,17 +80,16 @@ function checkCollision(newX, newY){
     }
     return false;
 }
-function mazeDraw(){
+function mazedraw(){
     for(let my = 0; my < 8; my++){
         for(let mx = 0; mx < 8; mx++){
             if(maze[my][mx]===1){
                 ctx.fillStyle = "#FFFFFF";
-                ctx.fillRect(mx * mazeSize - player.pos.x + (canvas.width / 2),
-                             my * mazeSize - player.pos.y + (canvas.height / 2),
-                             mazeSize,
-                             mazeSize);
+                ctx.fillRect(mx * mazeSize - player.pos.x + (canvas.width / 2) - 1,
+                             my * mazeSize - player.pos.y + (canvas.height / 2) - 1,
+                             mazeSize + 2,
+                             mazeSize + 2);
             }
         }
     }
 }
-

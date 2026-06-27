e1600a9e94a419b1b28ffbe465823ec8745c65d0
pukun1115-tech <pukun1115@gmail.com>
2026-06-27 12:12:07 +0900

    アイテム追加中

--- main.js
+++ main.js
@@ -37,6 +37,10 @@ canvas.addEventListener("touchend", (e) => {
     tap = false;
 });
 
+const items = {
+    key:false
+};
+
 loop();
 
 function loop(){
@@ -49,7 +53,7 @@ function loop(){
 }
 
 function move(){
-    let speed = keys["ShiftLeft"] ? 3 : 1.5;
+    let speed = keys["ShiftLeft"] ? 6 : 1.5;
     let nextPlayerX = player.pos.x;
     let nextPlayerY = player.pos.y;
     
@@ -86,34 +90,64 @@ function checkCollision(newX, newY){
 
     for(let my = 0; my < maze.length; my++){
         for(let mx = 0; mx < maze[0].length; mx++){
-            if(maze[my][mx]===1){
-                const wx = mx * mazeSize;
-                const wy = my * mazeSize;
-                if(
-                    newX < wx + mazeSize + 0.4 &&
-                    newX + player.size > wx - 0.4 &&
-                    newY < wy + mazeSize + 0.4 &&
-                    newY + player.size > wy - 0.4
-                ){
-                 return true;
+            const wx = mx * mazeSize;
+            const wy = my * mazeSize;
+            if(maze[my][mx] === 0) continue;
+            if(
+                newX < wx + mazeSize + 0.4 &&
+                newX + player.size > wx - 0.4 &&
+                newY < wy + mazeSize + 0.4 &&
+                newY + player.size > wy - 0.4
+            ){
+                if(maze[my][mx] === 1){
+                    return true;
+                }
+
+                if(maze[my][mx] === 2){
+                    items.key = true;
+                    maze[my][mx] = 0;
+                }
+
+                if(maze[my][mx] === 3)
+                {
+                    if(items.key){
+                        maze[my][mx] = 0;
+                    }
+                    else{
+                        return true;
+                    }
                 }
             }
         }
     }
     return false;
 }
+
 function mazedraw(){
     for(let my = 0; my < maze.length; my++){
         for(let mx = 0; mx < maze[0].length; mx++){
-            if(maze[my][mx] === 1){
-                ctx.fillStyle = "#808080";
-                ctx.fillRect(
-                    mx * mazeSize - player.pos.x + (canvas.width / 2) - 0.4,
-                    my * mazeSize - player.pos.y + (canvas.height / 2) - 0.4,
-                    mazeSize + 0.8,
-                    mazeSize + 0.8
-                );
+            if(maze[my][mx] === 0) continue;
+            if(maze[my][mx] === 1) ctx.fillStyle = "#808080";
+            if(maze[my][mx] === 2){
+                ctx.fillStyle = "#ffff00"
+
+                const r = (mazeSize / 2) * 0.5;
+                const cx = mx * mazeSize - player.pos.x + (canvas.width / 2) + mazeSize / 2;
+                const cy = my * mazeSize - player.pos.y +(canvas.height / 2) + mazeSize / 2;
+
+                ctx.beginPath();
+                ctx.arc(cx,cy,r,0,Math.PI * 2);
+                ctx.fill();
+                continue;
             }
+            ctx.fillRect(
+                mx * mazeSize - player.pos.x + (canvas.width / 2) - 0.4,
+                my * mazeSize - player.pos.y + (canvas.height / 2) - 0.4,
+                mazeSize + 0.8,
+                mazeSize + 0.8
+            );
+            
         }
     }
 }
+
